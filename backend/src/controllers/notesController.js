16dce5c7ab9c2f72d5876c7aa5f5efbe4e246984
Lalit Patil <lp613408@gmail.com>
2025-07-26 12:44:50 +0530

    feat: Finished backend with routes, MongoDB, and rate limiter

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,14 +1,26 @@
 import Note from "../models/Note.js"
 
-export async function getAllNotes(req, res) {
+export async function getAllNotes(_, res) {
     try {
-        const notes = await Note.find();
+        const notes = await Note.find().sort({createdAt: -1}); //newest first
         res.status(200).json(notes);
     } catch (error) {
         console.error("Error in gelAllNotes controller", error);
         res.status(500).json({ message: "Internal server error" });
     }
 };
+
+export async function getNoteById(req, res){
+    try {
+        const note = await Note.findById(req.params.id)
+        if(!note) return res.status(404).json({ message: "Note not found" });
+         res.status(200).json(note);   
+
+    } catch (error) {
+        console.error("Error in getNoteById controller", error);
+        res.status(500).json({ message: "Internal server error!" });
+    }
+}
 
 export async function createNote(req, res) {
     try {
@@ -45,12 +57,13 @@ export async function updateNote(req, res) {
 
 export async function deleteNote(req, res) {
    try {
-    
-    
-    //start from here 1:16:32 
-
+        // const {title, content} =
+        const deletedNote = await Note.findByIdAndDelete(req.params.id);
+        if(!deletedNote) return res.status(404).json({ message: "Note not found" });
+        res.status(200).json({message:"Note deleted successfully!"});
 
    } catch (error) {
-    
+        console.error("Error in DeleteNode controller", error);
+        res.status(500).json({ message: "Internal server error!" });    
    }
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,32 @@
 //import express = require("express");  //both are same 
 import express from 'express';  //If we write this & we get worning then add type:model in package.json 
-import notesRoutes from './routes/notesRoutes.js'; 
+import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
+
 
 //middleware
-app.use(express.json());
+app.use(express.json()); // This middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
 
-app.use("/api/notes", notesRoutes); 
- 
-app.listen(PORT, () => {
-    console.log("Server started on Port:",PORT);
+// our simple custom middleware
+// app.use((req,res,next) => {
+//     console.log(`Req method is ${req.method} $ Req URL is ${req.url}`);
+//     next();
+// });
+
+app.use("/api/notes", notesRoutes);
+
+// connect to database
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("Server started on Port:", PORT);
+    });
 });
