e48cc5bbc556ef9559df210a98efeeb9c917ab82
Lalit Patil <lp613408@gmail.com>
2025-07-26 19:20:39 +0530

    feat: implement Home Page with Navbar, NoteCard listing, and RateLimit handling

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,20 +1,28 @@
 //import express = require("express");  //both are same 
 import express from 'express';  //If we write this & we get worning then add type:model in package.json 
+import cors from "cors";
+import dotenv from "dotenv";
+
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import rateLimiter from './middleware/rateLimiter.js';
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-
-
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+    })
+);
 //middleware
 app.use(express.json()); // This middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
+
+
 
 // our simple custom middleware
 // app.use((req,res,next) => {
