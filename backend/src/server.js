a7c2a4a0f33ac3cf40af5c565003dd1204527d32
Lalit Patil <lp613408@gmail.com>
2025-07-28 00:04:44 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import express from 'express';  //If we write this & we get worning then add type:model in package.json 
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from "./config/db.js";
@@ -12,12 +13,16 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-    })
-);
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
 //middleware
 app.use(express.json()); // This middleware will parse JSON bodies: req.body
 app.use(rateLimiter);
@@ -31,6 +36,14 @@ app.use(rateLimiter);
 // });
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 // connect to database
 connectDB().then(() => {
